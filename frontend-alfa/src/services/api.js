1646b5e17131b74dd4e182be326025b4c79379d6
avelinofaco <facoavelino9@gmail.com>
2026-09-23 11:03:30 -0300

    fix: altera fallback do axios para a url do Render

--- frontend-alfa/src/services/api.js
+++ frontend-alfa/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Pega a URL do Render em produção ou usa localhost em desenvolvimento local
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://confeccao-alfa-api.onrender.com',
 });
 
 // Interceptador para anexa o Token JWT
